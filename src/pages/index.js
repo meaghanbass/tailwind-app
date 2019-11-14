e6cc657a7dd5ccbfadc16327dac8d0801f2c6042
Meaghan <meaghan@Nikkis-Mac-mini.local>
2019-11-14 07:28:58 -0800

    testing second computer

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default ({ data }) => {
         </h1>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-              <Link className="underline"
+              <Link className="underline bold"
               to={node.fields.slug}>
             <h3>
               {node.frontmatter.title}{" "}
